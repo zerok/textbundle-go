7c7fd50819d88ef43f7cb7bc83c485643c448d6d
Horst Gutmann <zerok@zerokspot.com>
2020-05-15 16:25:07 +0200

    Cleanup

--- reader.go
+++ reader.go
@@ -3,7 +3,7 @@ package textbundle
 import (
 	"archive/zip"
 	"encoding/json"
-	"io"
+	"fmt"
 	"io/ioutil"
 	"strings"
 )
@@ -14,26 +14,25 @@ func fn(name string) string {
 	return fnamePrefix + name
 }
 
+// Reader allows access the seperate Assets of a TextBundle, its content, and
+// the associated metadata.
 type Reader struct {
 	Metadata Metadata
 	Text     []byte
 	Assets   []Asset
 }
 
+// Asset is a light wrapper around zip.File that trims the filename.
 type Asset struct {
 	Name string
 	File *zip.File
 }
 
-func NewReader(r io.ReaderAt, size int64) (*Reader, error) {
-	zip.NewReader(r, size)
-	return nil, nil
-}
-
+// OpenReader creates a new Reader from the given file path.
 func OpenReader(path string) (*Reader, error) {
 	r, err := zip.OpenReader(path)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("could not open %s: %w", path, err)
 	}
 	bundle := &Reader{}
 	md := Metadata{}
@@ -41,22 +40,22 @@ func OpenReader(path string) (*Reader, error) {
 		if f.FileHeader.Name == fn("info.json") {
 			fp, err := f.Open()
 			if err != nil {
-				return nil, err
+				return nil, fmt.Errorf("could not open info.json for reading: %w", err)
 			}
 			if err := json.NewDecoder(fp).Decode(&md); err != nil {
 				fp.Close()
-				return nil, err
+				return nil, fmt.Errorf("could not decode info.json: %w", err)
 			}
 			fp.Close()
 		} else if strings.HasPrefix(f.FileHeader.Name, fnamePrefix+"text.") {
 			fp, err := f.Open()
 			if err != nil {
-				return nil, err
+				return nil, fmt.Errorf("could not open text item for reading: %w", err)
 			}
 			data, err := ioutil.ReadAll(fp)
 			if err != nil {
 				fp.Close()
-				return nil, err
+				return nil, fmt.Errorf("could not read text item: %w", err)
 			}
 			bundle.Text = data
 		} else if strings.HasPrefix(f.FileHeader.Name, fnamePrefix+"assets/") && f.FileHeader.Name != fn("assets/") {
